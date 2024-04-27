d8101671c30f355f6263a23dabfe2d397539ab1f
EduardoK-O <e.kuojeda@gmail.com>
2024-04-27 10:00:13 -0600

    login y token

--- mors-mutual-insurance-api/src/controller/UsuarioController.js
+++ mors-mutual-insurance-api/src/controller/UsuarioController.js
@@ -1,4 +1,5 @@
 const usuarioService = require("../services/UsuarioService");
+const jwt = require('jsonwebtoken');
 
 const getAllUsers = async (req, res) => {
     const allUsers = await usuarioService.getAllUsers();
@@ -50,10 +51,49 @@ const deleteUSer = async (req, res) => {
     return deletedUser;
 }
 
+const login = async (req, res) => {
+    const { body } = req;
+    if( !body.username || !body.password ){
+        return res.status(400).send('BAD REQUEST');
+    }
+    const data = {
+        username: body.username,
+        password: body.password
+    }
+    const user = (await usuarioService.login(data))[0];
+    console.log(user);
+    if (!user) {
+        return res.status(404).send("USER NOT FOUND");
+    }
+
+    const accessToken = generateAccessToken(user);
+    res.header('authorization', accessToken).json({message: "Usuario autenticado", token: accessToken});
+}
+
+function generateAccessToken(user){
+    console.log(process.env.SECRET);
+    return jwt.sign({user}, process.env.SECRET, {expiresIn: '30m'});
+}
+
+function validateToken(req, res, next){
+    const accessToken = req.headers['authorization'];
+    if(!accessToken) res.send('Access denied');
+
+    jwt.verify(accessToken, process.env.SECRET, (err, user)=>{
+        if(err){
+            res.status(401).send("Access denied, token expired or incorrect");
+        }else {
+            next();
+        }
+    });
+}
+
 module.exports = {
     getAllUsers,
     getUserById,
     createNewUser,
     updateUser,
-    deleteUSer
+    deleteUSer,
+    login,
+    validateToken
 }
